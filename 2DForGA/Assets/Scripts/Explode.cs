8848640cc4a9d9302c796b7176124f06c5196cca
Toni Pippola <toni.pippola@gmail.com>
2018-10-08 21:02:43 +0300

    Added exploding sript and bodypart prefab

--- 2DForGA/Assets/Scripts/Explode.cs
+++ 2DForGA/Assets/Scripts/Explode.cs
@@ -1,29 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class Explode : MonoBehaviour {
 
+	public BodyPart bodyPart;
+	public int totalParts;
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 
-	void OnTriggerEnter2D(Collider2D target) {
-		if(target.gameObject.tag == "Deadly") {
+	void OnTriggerEnter2D(Collider2D target){
+		if (target.gameObject.tag == "Deadly") {
 			OnExplode();
 		}
+
 	}
-	void OnExplode() {
 
-		//Application.LoadLevel(Application.loadedLevel);
+	void OnCollisionEnter2D(Collision2D target){
+		if (target.gameObject.tag == "Deadly") {
+			OnExplode();
+		}
 
+	}
+
+	public void OnExplode(){
 		Destroy (gameObject);
-	}
-}
 
+		var t = transform;
+
+		for (int i = 0; i < totalParts; i++) {
+			BodyPart clone = Instantiate(bodyPart, t.position, Quaternion.identity) as BodyPart;
+			clone.GetComponent<Rigidbody2D>().AddForce(Vector3.right * (Random.Range (-50, 50)));
+			clone.GetComponent<Rigidbody2D>().AddForce(Vector3.up * Random.Range(100, 400));
+		}
+
+
+	}
+
+}
