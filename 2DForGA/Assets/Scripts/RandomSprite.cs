0427d693cc75545d4849256c5cbae22296965f4c
Toni Pippola <toni.pippola@gmail.com>
2018-10-08 18:33:06 +0300

    Crystal base & more code

---
+++ 2DForGA/Assets/Scripts/RandomSprite.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSprite : MonoBehaviour {
+
+	public Sprite[] sprites;
+	public string resourceName;
+
+	// Use this for initialization
+	void Start () {
+		if (resourceName != "") {
+			sprites = Resources.LoadAll<Sprite> (resourceName);
+			GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	// Use this for initialization
+
+}
